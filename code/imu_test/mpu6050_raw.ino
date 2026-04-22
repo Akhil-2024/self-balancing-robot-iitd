48065d30c98b798999afb279b03fee1cfb736537
Akhil-2024 <113796833+Akhil-2024@users.noreply.github.com>
2026-04-22 19:35:17 +0530

    Update mpu6050_raw.ino

--- code/imu_test/mpu6050_raw.ino
+++ code/imu_test/mpu6050_raw.ino
@@ -1,1 +1,38 @@
+#include <Wire.h>
+#include "I2Cdev.h"
+#include "MPU6050.h"
 
+MPU6050 mpu;
+
+int16_t ax, ay, az;
+int16_t gx, gy, gz;
+
+void setup() {
+  Serial.begin(9600);
+  Wire.begin();
+
+  mpu.initialize();
+
+  // Do NOT use mpu.testConnection() now
+  Serial.println("Reading raw MPU6050 data...");
+}
+
+void loop() {
+  mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+
+  Serial.print("AX: ");
+  Serial.print(ax);
+  Serial.print(" AY: ");
+  Serial.print(ay);
+  Serial.print(" AZ: ");
+  Serial.print(az);
+
+  Serial.print(" | GX: ");
+  Serial.print(gx);
+  Serial.print(" GY: ");
+  Serial.print(gy);
+  Serial.print(" GZ: ");
+  Serial.println(gz);
+
+  delay(200);
+}
